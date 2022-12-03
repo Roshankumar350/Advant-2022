b54c9c1dd8433ae8638f08e9394b70b11240c10a
Roshan Kumar Sah <roshankumar350@gmail.com>
2022-12-04 00:52:36 +0530

    Files and folder setup

---
+++ Advant-2022/Advant-2022/Day3/Data3.swift
@@ -0,0 +1,8 @@
+//
+//  Data3.swift
+//  Advant-2022
+//
+//  Created by Roshan sah on 04/12/22.
+//
+
+import Foundation
---
+++ Advant-2022/Advant-2022/Day3/Logic3.swift
@@ -0,0 +1,8 @@
+//
+//  Logic3.swift
+//  Advant-2022
+//
+//  Created by Roshan sah on 04/12/22.
+//
+
+import Foundation
